5e8e70bfc4e578ff5571992dfc4bd5dc9fb490c9
pnoga190401 <paulinanoga55@gmail.com>
2018-10-24 10:34:37 +0200

    euklides2

--- cpp/euklides.cpp
+++ cpp/euklides.cpp
@@ -8,19 +8,40 @@
 
 using namespace std;
 
+int nwd_optymalny(int a, int b) {
+    int licznik = 0;
+    while(a > 0) {
+        a = a % b;
+        b = b - a;
+        licznik++;
+    }
+    cout << "Powtórzeń: " << licznik << endl;
+    return b;
+}
 
-
-
+int nwd_klasyczny(int a, int b) {
+    int licznik = 0;
+    while(a > b) {
+        if(a > b)
+            a = a - b;
+        else
+            b = b - a;
+        licznik++;
+    }
+    cout << "Powtórzeń: " << licznik << endl;
+    return a;
+}
 
 int main(int argc, char **argv)
 {
 	int a, b;
     cout << "Podaj 2 liczby: ";
     cin >> a >> b;
-    inr nwd = nwd1(a, b);
+    int nwd = nwd_klasyczny(a, b); 
     cout << "NWD (" << a << ","
-    << b << ") = "
-    << nwd << endl;
+        << b << ") = "
+        << nwd << endl;
+    nwd = nwd_optymalny(a, b);
     
 	return 0;
 }
---
+++ cpp/min_max/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+void minmax1() {
+    char dalej = 't';
+    int liczba = 0;
+    int min = 0;
+    while (dalej = 't') {
+        cout << "podaj liczbe :";
+    }
+}
+
+
+int main()
+{
+    minmax1();
+    return 0;
+}
