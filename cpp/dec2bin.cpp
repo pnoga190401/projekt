c53b317672a1d37df97a996b58cbb05a54db8caf
pnoga190401 <paulinanoga55@gmail.com>
2019-02-04 07:20:10 +0100

    quiz

---
+++ cpp/dec2bin.cpp
@@ -0,0 +1,25 @@
+/*
+ * dec2bin.cpp 
+ */
+
+using namespace std;
+#include <iostream>
+
+void dec2bin(int l){
+    ; 
+}
+
+
+void bin2dec(int tab[]){
+   ; 
+}
+
+
+
+
+int main(int argc, char **argv)
+{
+	
+	return 0;
+}
+
---
+++ cpp/nieparzyste.cpp
@@ -0,0 +1,24 @@
+/*
+ * nieparzyste.cpp 
+ */
+
+using namespace std;
+
+#include <iostream>
+
+
+
+
+int main(int argc, char **argv)
+{
+	int n;
+    cout << "Podaj liczbę: ";
+    cin >> n;
+    
+    for (i = 1; ; i+2){
+        
+    }
+    
+	return 0;
+}
+
